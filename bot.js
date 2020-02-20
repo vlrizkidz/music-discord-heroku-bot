3fc4b53353a408c9270d8daa617ddacfba1382ae
vlrizkidz <vlrizkidz@gmail.com>
2020-02-21 06:10:21 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,10 +18,10 @@ bot.music.start(bot, {
 });
 
 // Events.
-*bot.on("ready", function() {
-*    bot.user.setActivity(`in Dream`);
-*    console.log(`${bot.user.username} is Ready!`);
-*});
+//bot.on("ready", function() {
+//    bot.user.setActivity(`in Dream`);
+//    console.log(`${bot.user.username} is Ready!`);
+//});
 
 client.on("ready", () => {
   const channel = client.channels.get("561532573868752916");
