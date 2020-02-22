658c58142b79c9a2de5a1acedc9eda8914efca28
vlrizkidz <vlrizkidz@gmail.com>
2020-02-22 16:50:28 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -51,7 +51,26 @@ client.on('message', message => {
   //...
 });
 
+const client = new Discord.Client();
+module.exports = async (oldMember,newMember,message) => {
 
+    if(newMember.user.bot) return;
+    let newUserChannel = newMember.voiceChannel
+    let oldUserChannel = oldMember.voiceChannel
+
+    let newdate = new Date(newMember.joinedTimestamp)
+
+    var log =  message.guild.channels.find(ch => ch.name.includes('voice-log')) || message.guild.channels.find(ch => ch.name.includes('member-log')) || message.guild.channels.find(ch => ch.name.includes('log')) || message.guild.channels.find(ch => ch.name.includes('logs')) || message.guild.channels.find(ch => ch.name.includes('general'));
+
+    if(oldUserChannel === undefined && newUserChannel !== undefined) {
+        log.send(`:small_red_triangle_down: <@${newMember.user.id}> Left a voice channel at ${newdate}`); 
+    } else if(newUserChannel === undefined){
+        log.send(`:white_check_mark: <@${newMember.user.id}> Joined a voice channel at ${newdate}`);
+    }else if(oldUserChannel !== newUserChannel){
+        log.send(`:arrow_right: <@${newMember.user.id}> Joined a voice channel at ${newdate}`);
+    }
+
+    }
 
 //bot.on('voiceStateUpdate', (oldMember, newMember) => {
 //  let newUserChannel = newMember.voiceChannel
