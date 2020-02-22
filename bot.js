e13a5ca2e07dab4412f3729c52289bee5cb7cba4
vlrizkidz <vlrizkidz@gmail.com>
2020-02-22 19:21:05 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,15 +2,15 @@ const Discord = require("discord.js"); // We Call The Packages.
 // const PREFIX = "<"; // You can change this Prefix to whatever you want.
 const PREFIX = process.env.PREFIX;
 
-const bot = new Discord.Client();
+const client = new Discord.Client();
 
 // Put the Music module in the new Client object.
 // This allows for easy access to all the modules
 // functions and data.
-bot.music = require("discord.js-musicbot-addon");
+client.music = require("discord.js-musicbot-addon");
 
 // Now we start the music module.
-bot.music.start(bot, {
+client.music.start(bot, {
     // Set the api key used for YouTube.
     // This is required to run the bot.
     youtubeKey: process.env.BOT_YOUTUBE_TOKEN,
@@ -18,13 +18,13 @@ bot.music.start(bot, {
 });
 
 // Events.
-bot.on("ready", function() {
-    bot.user.setActivity(`hell yeah`);
-    console.log(`${bot.user.username} is Ready!`);
-});
+//client.on("ready", function() {
+//    bot.user.setActivity(`hell yeah`);
+//    console.log(`${bot.user.username} is Ready!`);
+//});
 
-bot.on("ready", () => {
-  const channel = bot.channels.get("561245349818269696");
+client.on("ready", () => {
+  const channel = client.channels.get("665123932881551360");
   if (!channel) return console.error("The channel does not exist!");
   channel.join().then(connection => {
     // Yay, it worked!
