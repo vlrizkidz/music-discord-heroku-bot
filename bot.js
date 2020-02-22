dadcf5a5f4ff3c3ab1d123060e66d15e1227213d
vlrizkidz <vlrizkidz@gmail.com>
2020-02-22 19:05:49 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
 const Discord = require("discord.js"); // We Call The Packages.
 // const PREFIX = "<"; // You can change this Prefix to whatever you want.
 const PREFIX = process.env.PREFIX;
-const client = new Discord.Client();
-var bot = new Discord.Client();
+
+const bot = new Discord.Client();
 
 // Put the Music module in the new Client object.
 // This allows for easy access to all the modules
@@ -19,45 +19,10 @@ bot.music.start(bot, {
 
 // Events.
 //bot.on("ready", function() {
-//    bot.user.setActivity(`in Dream`);
+//    bot.user.setActivity(`Hi, Im The Music Bot`);
 //    console.log(`${bot.user.username} is Ready!`);
 //});
 
 // Bot Login.
 // bot.login('YourAwesomeBotToken');
 bot.login(process.env.BOT_TOKEN);
-
-
-//576876993166180362
-//561280914861326367
-//561285091440066564
-//561285183039602696
-//561285289507815424
-//561802892030377994
-
-bot.on('ready', () => {
-	console.log('Ready Cuy!');
-});
-
-
-module.exports = async (oldMember, newMember, message) => {
-    if(newState.user.bot) return;
-    let newUserChannel = newMember.voiceChannel
-    let oldUserChannel = newMember.voiceChannel
-    console.log(oldMember,newMember);
-
-    let newdate = new Date(newState.joinedTimestamp)
-
-    var log =  message.guild.channels.find(ch => ch.name.includes('nothing')) || message.guild.channels.find(ch => ch.name.includes('bot')) || message.guild.channels.find(ch => ch.name.includes('empty'));
-
-    if(oldUserChannel === undefined && newUserChannel !== undefined) {
-        log.send(`:small_red_triangle_down: <@${newMember.user.id}> Left a voice channel at ${newdate}`); 
-    } else if(newUserChannel === undefined){
-        log.send(`:white_check_mark: <@${newMember.user.id}> Joined a voice channel at ${newdate}`);
-    }else if(oldUserChannel !== newUserChannel){
-        log.send(`:arrow_right: <@${newMember.user.id}> Joined a voice channel at ${newdate}`);
-    }
-
-    }
-
-process.on("unhandledRejection", console.error);
